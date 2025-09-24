494aa056fa738321419da0a3a23ed8086b3d74bb
alvalavlavanderia <alvalavlavanderia@gmail.com>
2025-09-24 14:51:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,9 +109,3 @@ def insert_servico(desc):
         conn.commit()
         st.success("Serviço cadastrado com sucesso!")
     except sqlite3.IntegrityError:
-        st.error("Erro: Serviço já cadastrado.")
-    finally:
-        conn.close()
-
-def insert_usuario(usuario, senha, is_admin_flag):
-    conn
